b3bbc429b60111f4f21fa28610f9d4a84e75314e
fallsmohammed <40745423+fallsmohammed@users.noreply.github.com>
2018-07-03 01:25:24 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,4 +28,43 @@ client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
   console.log('')
 });
 
+
+
+//serv-avatar
+client.on("message", message => {    
+    if(!message.channel.guild) return;
+if(message.author.bot) return;
+if(message.content === "!>ser-av"){ 
+    const embed = new Discord.RichEmbed()
+
+.setTitle(`صورة ** ${message.guild.name} **`)
+.setAuthor(message.author.username, message.guild.iconrURL)
+.setColor('RANDOM')
+.setImage(message.guild.iconURL)
+
+message.channel.send({embed});
+}
+});
+
+//avatar
+client.on('message', message => {
+if (message.content.startsWith("!>avatar")) {
+  var mentionned = message.mentions.users.first();
+var x5bzm;
+if(mentionned){
+    var x5bzm = mentionned;
+} else {
+    var x5bzm = message.author;
+    
+}
+  const embed = new Discord.RichEmbed()
+  .setColor("RANDOM")
+  .setImage(`${x5bzm.avatarURL}`)
+message.channel.sendEmbed(embed);
+}
+});
+
+
+
+
 client.login(process.env.BOT_TOKEN);
