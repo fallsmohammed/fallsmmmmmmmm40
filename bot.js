f3d6adeb78f145903c272e15995cd393db6879ab
fallsmohammed <40745423+fallsmohammed@users.noreply.github.com>
2018-07-03 20:25:57 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -139,14 +139,7 @@ client.on('message', message => {
 
 
 
-js
-client.on("guildMemberAdd", member => {
-  member.createDM().then(function (channel) {
-  return channel.send(`:rose:  ولكم نورت السيرفر:rose: 
-:crown:اسم العضو  ${member}:crown:  
-انت العضو رقم ${member.guild.memberCount} `) 
-}).catch(console.error)
-})
+
 
 
 
