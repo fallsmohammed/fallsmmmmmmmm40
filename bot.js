619f789c9d5f6247e61a41dc68bc44c565572b68
fallsmohammed <40745423+fallsmohammed@users.noreply.github.com>
2018-07-22 04:54:45 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -154,14 +154,6 @@ client.on("guildMemberAdd", member => {
 });
 
 
- client.on('message', message => {
-    if (message.content === '+roles') {
-        var roles = message.guild.roles.map(roles => `${roles.name}, `).join(' ')
-        const embed = new Discord.RichEmbed()
-        .setColor('RANDOM')
-        .addField('الرتب:',`**[${roles}]**`)
-        message.channel.sendEmbed(embed);
-    }
-});
+
 
 client.login(process.env.BOT_TOKEN);
